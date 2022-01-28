17d2b61592a90fc2e3abdf2a817168fb197d30ee
buezrico <ricobuez@gmail.com>
2022-01-28 11:35:29 +0100

    added adresses

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -23,7 +23,7 @@ const LogIn = () => {
     <Wrapper>
       <div className="container login">
         <GoogleLogin
-          clientId="1053308372239-q514o398rd9f36vtcan3jah78duksv2t.apps.googleusercontent.com"
+          clientId="829312939207-0mtq5de7pfjj0cioq6ilf6ajvjrih18h.apps.googleusercontent.com"
           onSuccess={onLoginSuccess}
           onFailure={(e) => console.log(e)}
           disabled={Boolean(user)}
